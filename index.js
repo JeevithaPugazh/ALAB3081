d1ea7c8eee88629c9d8ef9a7aece705dd74ba2a8
Jeevitha <jeevitha.pugazh@gmail.com>
2025-02-26 16:02:10 -0500

    Third commit

--- index.js
+++ index.js
@@ -2,28 +2,22 @@
 console.log("--- > PART 1 < ---");
 let n1 = 10;
 let n2 = 15;
-let n3 = 12;
-let n4 = 13;
+let n3 = 20;
+let n4 = 5;
 
 //Checking the sum of four number is true
-let sumCheck = n1 + n2 + n3 + n4 === 50;
-console.log(`Is sum is 50?:${sumCheck}`);
+let isSum50 = n1 + n2 + n3 + n4 === 50;
+
 
 // Checking at least two value is odd
-let checkOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2);
-let checkAtleastTwoValueIsOdd = checkOdd >= 2;
-console.log(
-  `Is at least two value is odd?: ${checkAtleastTwoValueIsOdd}`
-);
+let isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2);
+let checkAtleastTwoValueIsOdd = isTwoOdd >= 2;
 
-// checking is all numbers are smaller than 25
-let withInRange = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
-console.log(
-  `Is all numbers are smaller than 25? : ${withInRange}`
-);
+
+
 
 //Checking is all numbers are unique
-let checkUnique =
+let isUnique =
   n1 !== n2 &&
   n1 !== n3 &&
   n1 !== n3 &&
@@ -32,17 +26,17 @@ let checkUnique =
   n2 !== n3 &&
   n2 !== n4 &&
   n3 !== n4;
-console.log(`Is all numbers are unique?: ${checkUnique}`);
+
 
 //Check all Values are divisible by 5
-let areAllDivisibleBy5 =
+let isDivisibleBy5 =
   n1 % 5 === 0 &&
   n2 % 5 === 0 &&
   n3 % 5 === 0 &&
   n4 % 5 === 0;
 
 console.log(
-  `Is all numbers are divisible by 5?: ${areAllDivisibleBy5}`
+  `Is all numbers are divisible by 5?: ${isDivisibleBy5}`
 );
 
 //Check if the first number is larger than the last
@@ -53,7 +47,7 @@ console.log(
 
 //Arithmetic chain
 //Subtract the first number from the second number.
-let subFirstNumAndLastNum = n1 - n4;
+let subFirstNumAndLastNum = n2-n1;
 console.log(
   `Result of subtracting First number from the second number: ${subFirstNumAndLastNum}`
 );
@@ -67,17 +61,21 @@ console.log(
 
 // Find the remainder of dividing the result by the fourth number.
 let divideMulResultByFourthNumber =
-  multiplySubResultByThirdNumber / n4;
+  multiplySubResultByThirdNumber % n4;
 console.log(
-  `Result of dividing the multiplication result by fourth number: ${divideMulResultByFourthNumber}`
+  `Remainder of dividing the multiplication result by fourth number: ${divideMulResultByFourthNumber}`
 );
 
-//checking result are above 25 using isOver25
-let isOver25 =
-  subFirstNumAndLastNum > 25 ||
-  multiplySubResultByThirdNumber > 25 ||
-  divideMulResultByFourthNumber > 25;
-console.log(`Is any number over 25?: ${isOver25}`);
+
+// checking is all numbers are smaller than 25
+let isBelow25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
+console.log(
+  `Is all numbers are smaller than 25? : ${isBelow25}`
+);
+
+//
+const isValid = isSum50 && isTwoOdd && isBelow25 && isUnique;
+console.log(isValid)
 
 // Part 2: Practical Math
 console.log("");
@@ -120,48 +118,47 @@ let bestSpeed =
 
 console.log("--- If a person travelling in 55mph ---");
 console.log(
-  `Total fuel needed:${fuelNeeded1.toFixed(2)} gallons`
+  `Total fuel needed:${fuelNeeded1} gallons`
 );
 console.log(
-  `Total fuel cost:$${totalFuelCost1.toFixed(2)}`
+  `Total fuel cost:$${totalFuelCost1}`
 );
 console.log(
   `Is the budget enough to cover the fuel expenses?: ${isWithinBudget1}`
 );
 console.log(
-  `Total trip duration: ${tripDuration1.toFixed(2)} hours`
+  `Total trip duration: ${tripDuration1} hours`
 );
 
 console.log("");
 console.log("--- If a person travelling in 60mph ---");
 console.log(
-  `Total fuel needed:${fuelNeeded2.toFixed(2)} gallons`
+  `Total fuel needed:${fuelNeeded2} gallons`
 );
 console.log(
-  `Total fuel cost:$${totalFuelCost2.toFixed(2)}`
+  `Total fuel cost:$${totalFuelCost2}`
 );
 console.log(
   `Is the budget enough to cover the fuel expenses?: ${isWithinBudget2}`
 );
 console.log(
-  `Total trip duration: ${tripDuration2.toFixed(2)} hours`
+  `Total trip duration: ${tripDuration2} hours`
 );
 
 console.log("");
 console.log("--- If a person travelling in 75mph ---");
 console.log(
-  `Total fuel needed:${fuelNeeded3.toFixed(2)} gallons`
+  `Total fuel needed:${fuelNeeded3} gallons`
 );
 console.log(
-  `Total fuel cost:$${totalFuelCost3.toFixed(2)}`
+  `Total fuel cost:$${totalFuelCost3}`
 );
 console.log(
   `Is the budget enough to cover the fuel expenses?: ${isWithinBudget3}`
 );
 console.log(
-  `Total trip duration: ${tripDuration3.toFixed(2)} hours`
+  `Total trip duration: ${tripDuration3} hours`
 );
 console.log("");
 console.log(`The best speed for this trip is : ${bestSpeed}`);
 
-console.log("--- PART 3 ---")
